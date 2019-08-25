3354475599e57e0b99b29d7b6b30117cb1022c97
mwoss <wos.mateusz16@gmail.com>
2019-08-25 23:59:36 +0200

    depth algo

---
+++ leet_code/easy/maximus_depth_tree.py
@@ -0,0 +1,36 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepthBFS(self, root: 'Node') -> int:
+        # BFS Solution
+        if root is None:
+            return 0
+
+        stack = [(root, 1)]
+        max_depth = 0
+
+        while stack:
+            node, height = stack.pop()
+            max_depth = max(max_depth, height)
+
+            if node.children:
+                for child in node.children:
+                    stack.append((child, height + 1))
+
+        return max_depth
+
+    def maxDepthDFS(self, root: 'Node') -> int:
+        max_depth = 0
+
+        if root:
+            max_depth += 1
+
+        if root and root.children:
+            max_depth += max([self.maxDepthDFS(child) for child in root.children])
+
+        return max_depth
