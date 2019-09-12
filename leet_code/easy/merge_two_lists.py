0dc6cd8534ea813efe5272eb9ef191ae9d193df1
mwoss <wos.mateusz16@gmail.com>
2019-09-12 23:40:06 +0200

    next algo

---
+++ leet_code/easy/merge_two_lists.py
@@ -0,0 +1,34 @@
+"""
+Merge two sorted linked lists and return it as a new list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = current = ListNode(None)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        current.next = (l1 or l2)
+
+        return result.next
