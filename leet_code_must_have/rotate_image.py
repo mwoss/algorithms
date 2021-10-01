86b9c31b8e6339962ee268e053846710533dc216
mwoss <wos.mateusz16@gmail.com>
2021-10-01 23:33:02 +0200

    algo

---
+++ leet_code_must_have/rotate_image.py
@@ -0,0 +1,25 @@
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+DO NOT allocate another 2D matrix and do the rotation.
+"""
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):  # each rotation round
+            for j in range(n // 2):  # each inner matrix
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n - j - 1][i]
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+                matrix[j][n - i - 1] = temp
+
+
+if __name__ == '__main__':
+    s = Solution()
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    s.rotate(matrix)
+    print(matrix)
