f360ae6d766ea05142fca3fffae8433da990233b
mwoss <wos.mateusz16@gmail.com>
2021-10-02 00:23:21 +0200

    algo

--- leet_code_must_have/rotate_image.py
+++ leet_code_must_have/rotate_image.py
@@ -18,6 +18,24 @@ class Solution:
                 matrix[j][n - i - 1] = temp
 
 
+class Solution2:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reverse(matrix)
+
+    def transpose(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    def reverse(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
+
+
 if __name__ == '__main__':
     s = Solution()
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
