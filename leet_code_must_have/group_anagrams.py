ae9aa1adaa75d7d904b3a4b99dc872e2908fab8f
mwoss <wos.mateusz16@gmail.com>
2021-10-03 20:42:31 +0200

    refactor solution

--- leet_code_must_have/group_anagrams.py
+++ leet_code_must_have/group_anagrams.py
@@ -2,38 +2,40 @@ from collections import defaultdict
 from typing import List
 
 
-def group_anagrams(words: List[str]) -> List[List[str]]:
-    groups = defaultdict(list)
-    for word in words:
-        bucket = "".join(sorted(word))
-        groups[bucket].append(word)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for word in strs:
+            bucket = "".join(sorted(word))
+            groups[bucket].append(word)
 
-    return list(groups.values())
-
-
-def group_anagrams_count_sort(words: List[str]) -> List[List[str]]:
-    groups = defaultdict(list)
-    counters = [0] * 26
-    for word in words:
-        bucket = counting_sort_words(word, counters)
-        groups[bucket].append(word)
-
-    return list(groups.values())
+        return list(groups.values())
 
 
-def counting_sort_words(word: str, counters: list) -> str:
-    for letter in word:
-        counters[ord(letter) - 97] += 1
+class Solution2:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        counters = [0] * 26
+        for word in strs:
+            bucket = self._counting_sort_words(word, counters)
+            groups[bucket].append(word)
 
-    output = ""
-    for idx, count in enumerate(counters):
-        if count > 0:
-            output += chr(idx + 97) * count
-            counters[idx] = 0
+        return list(groups.values())
 
-    return output
+    def _counting_sort_words(self, word: str, counters: list) -> str:
+        for letter in word:
+            counters[ord(letter) - 97] += 1
+
+        output = ""
+        for idx, count in enumerate(counters):
+            if count > 0:
+                output += chr(idx + 97) * count
+                counters[idx] = 0
+
+        return output
 
 
 if __name__ == '__main__':
+    s = Solution2()
     words = ["aew", "wea", "lool", "ooll", "xd"]
-    print(group_anagrams_count_sort(words))
+    print(s.groupAnagrams(words))
