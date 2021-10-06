b16530cc8778ecde468955c9786693792fecd4db
mwoss <wos.mateusz16@gmail.com>
2021-10-06 02:05:45 +0200

    algos

--- leet_code_must_have/tree_level_order_traversal.py
+++ leet_code_must_have/tree_level_order_traversal.py
@@ -2,7 +2,7 @@
 Given the root of a binary tree, return the level order traversal of its nodes' values.
 (i.e., from left to right, level by level).
 """
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List, Optional, Dict
 
 
@@ -33,16 +33,39 @@ class Solution2:
 
         stack, result = [(root, 0)], defaultdict(list)
         while stack:
-            node, level = stack.pop(0)
+            node, level = stack.pop(0)  # it's better to use deque for BFS, as pop(0) has O(n) complexity
             if node is not None:
                 result[level].append(node.val)
-                stack.append((node.left, level+1))
-                stack.append((node.right, level+1))
+                stack.append((node.left, level + 1))
+                stack.append((node.right, level + 1))
 
         return list(result.values())
 
 
+class Solution3:
+    def level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        queue, result = deque([root]), []
+        while queue:
+            curr_level, curr_size = [], len(queue)
+            for _ in range(curr_size):
+                node = queue.popleft()
+
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+
+                curr_level.append(node.val)
+
+            result.append(curr_level)
+
+        return result
+
+
 if __name__ == '__main__':
-    s = Solution2()
+    s = Solution3()
     tree = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
     print(s.level_order(tree))
