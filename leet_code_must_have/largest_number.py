966f5f82544a8537143cf891b1b908bf219f6ad7
mwoss <wos.mateusz16@gmail.com>
2021-10-17 23:23:22 +0200

    algo

--- leet_code_must_have/largest_number.py
+++ leet_code_must_have/largest_number.py
@@ -8,14 +8,15 @@ from typing import List
 
 class Solution:
     def largest_number(self, nums: List[int]) -> str:
-        self._quick_sort(nums, 0, len(nums) - 1)
+        str_nums = [str(num) for num in nums]
+        self._quick_sort(str_nums, 0, len(nums) - 1)
 
         if nums[0] == 0:
             return "0"
 
-        return "".join(str(num) for num in nums)
+        return "".join(str_nums)
 
-    def _quick_sort(self, nums: List[int], begin: int, end: int):
+    def _quick_sort(self, nums: List[str], begin: int, end: int):
         if begin >= end:
             return
 
@@ -23,39 +24,49 @@ class Solution:
         self._quick_sort(nums, begin, part - 1)
         self._quick_sort(nums, part + 1, end)
 
-    def _partition(self, nums: List[int], begin: int, end: int) -> int:
+    def _partition(self, nums: List[str], begin: int, end: int) -> int:
         pivot = begin
-        while begin < end:
-            if self._compare(nums[begin], nums[end]):
-                nums[begin], nums[pivot] = nums[pivot], nums[begin]
+
+        # while begin < end:
+        #     if self._compare(nums[begin], nums[end]):
+        #         nums[begin], nums[pivot] = nums[pivot], nums[begin]
+        #         pivot += 1
+        #     begin += 1
+        # nums[end], nums[pivot] = nums[pivot], nums[end]
+
+        for i in range(begin + 1, end + 1):
+            if self._compare(nums[i], nums[pivot]):
                 pivot += 1
-            begin += 1
-        nums[end], nums[pivot] = nums[pivot], nums[end]
+                nums[i], nums[pivot] = nums[pivot], nums[i]
+
+        nums[pivot], nums[begin] = nums[begin], nums[pivot]
+
         return pivot
 
-    def _compare(self, e1: int, e2: int) -> bool:
-        return str(e1) + str(e2) > str(e2) + str(e1)
+    def _compare(self, e1: str, e2: str) -> bool:
+        return e1 + e2 > e2 + e1
 
 
 class Solution2:
     def largest_number(self, nums: List[int]) -> str:
-        nums.sort(key=cmp_to_key(self._compare))
+        str_nums = [str(num) for num in nums]
+        str_nums.sort(key=cmp_to_key(self._compare))
 
-        if nums[0] == 0:
+        if str_nums[0] == "0":
             return "0"
 
-        return "".join(str(num) for num in nums)
+        return "".join(str_nums)
 
     @staticmethod
-    def _compare(e1: int, e2: int) -> int:
-        if str(e1) + str(e2) < str(e2) + str(e1):
+    def _compare(e1: str, e2: str) -> int:
+        if e1 + e2 < e2 + e1:
             return 1
-        elif str(e1) + str(e2) > str(e2) + str(e1):
+        elif e1 + e2 > e2 + e1:
             return -1
         return 0
 
 
 if __name__ == '__main__':
-    s = Solution2()
+    s = Solution()
     print(s.largest_number([10, 2]))
     print(s.largest_number([3, 30, 34, 5, 9]))
