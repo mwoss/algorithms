eb27415958f74dd675f20dbd03b30370c3758bda
mwoss <wos.mateusz16@gmail.com>
2021-11-20 18:06:52 +0100

    algo ver1

--- interviews/s/language_header.py
+++ interviews/s/language_header.py
@@ -2,3 +2,9 @@
 Language HTTP header parsing.
 
 """
+from typing import List, Set
+
+
+def get_accepted_languages(user_language_header: str, server_acceptable_languages: Set[str]) -> List[str]:
+    parsed_user_langs = [lang.strip() for lang in user_language_header.split(",")]
+    return [lang for lang in parsed_user_langs if lang in server_acceptable_languages]
