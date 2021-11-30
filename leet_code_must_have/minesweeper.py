ec84c3c37eb8408f4bb02cdde6ac684340383c87
mwoss <wos.mateusz16@gmail.com>
2021-11-30 02:09:00 +0100

    ugly but working

--- leet_code_must_have/minesweeper.py
+++ leet_code_must_have/minesweeper.py
@@ -8,13 +8,16 @@ You are given an m x n char matrix board representing the game board where:
 'B' represents a revealed blank square that has no adjacent mines (i.e., above, below, left, right, and all 4 diagonals),
 digit ('1' to '8') represents how many mines are adjacent to this revealed square, and
 'X' represents a revealed mine.
-You are also given an integer array click where click = [clickr, clickc] represents the next click position among all the unrevealed squares ('M' or 'E').
+You are also given an integer array click where click = [clickr, clickc] represents the next click position
+among all the unrevealed squares ('M' or 'E').
 
 Return the board after revealing this position according to the following rules:
 
 If a mine 'M' is revealed, then the game is over. You should change it to 'X'.
-If an empty square 'E' with no adjacent mines is revealed, then change it to a revealed blank 'B' and all of its adjacent unrevealed squares should be revealed recursively.
-If an empty square 'E' with at least one adjacent mine is revealed, then change it to a digit ('1' to '8') representing the number of adjacent mines.
+If an empty square 'E' with no adjacent mines is revealed, then change it to a revealed blank 'B' and all of its
+adjacent unrevealed squares should be revealed recursively.
+If an empty square 'E' with at least one adjacent mine is revealed, then change it to a digit ('1' to '8')
+representing the number of adjacent mines.
 Return the board when no more squares will be revealed.
 """
 
@@ -36,10 +39,10 @@ class Solution:
     def _reveal_cells(self, board: List[List[str]], cords: List[int]):
         x, y = cords
 
-        if 0 > x < len(board) or 0 > y < len(board[0]):
+        if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
             return
 
-        if board[x][y] == "M":
+        if board[x][y] != "E":
             return
 
         mines = self._count_mines_nearby(board, cords)
@@ -48,6 +51,9 @@ class Solution:
             board[x][y] = "B"
         else:
             board[x][y] = str(mines)
+
+        if board[x][y] != "B":
+            return
 
         self._reveal_cells(board, [x + 1, y])
         self._reveal_cells(board, [x - 1, y])
@@ -87,4 +93,6 @@ if __name__ == '__main__':
     board = [["E", "E", "E", "E", "E"], ["E", "E", "M", "E", "E"], ["E", "E", "E", "E", "E"], ["E", "E", "E", "E", "E"]]
     click = [3, 0]
     s = Solution()
-    print(s.update_board(board, click))
+    import pprint
+
+    pprint.pprint(s.update_board(board, click))
