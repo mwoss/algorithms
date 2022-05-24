24d1f7f5b766e4fdecd5835f6b4e605798220f67
mwoss <wos.mateusz16@gmail.com>
2022-05-25 00:03:49 +0200

    Refactor solution a bit

--- blind75/two_sum.py
+++ blind75/two_sum.py
@@ -3,13 +3,12 @@ from typing import List
 
 class Solution:
     def two_sum(self, nums: List[int], target: int) -> List[int]:
-        buffer = {}
+        number_to_index = {}
         for idx, num in enumerate(nums):
-            desire = target - num
-            if desire not in buffer:
-                buffer[num] = idx
-            else:
-                return [idx, buffer[desire]]
+            to_find = target - num
+            if to_find in number_to_index:
+                return [number_to_index[to_find], idx]
+            number_to_index[num] = idx
 
         return [-1, -1]
 
