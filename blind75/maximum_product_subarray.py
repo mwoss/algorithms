c0e792ef81a32a8fe8a9077705dab7f96667e618
mwoss <wos.mateusz16@gmail.com>
2022-06-21 01:07:39 +0200

    Add solution for max product subarray problem

--- blind75/maximum_product_subarray.py
+++ blind75/maximum_product_subarray.py
@@ -15,7 +15,17 @@ def max_product(nums: List[int]) -> int:
     return max(nums)
 
 
+def max_product_correct(nums: List[int]) -> int:
+    min_prod, max_prod, result = nums[0], nums[0], nums[0]
+    for i in range(1, len(nums)):
+        temp_min = min(nums[i], min_prod * nums[i], max_prod * nums[i])
+        temp_max = max(nums[i], min_prod * nums[i], max_prod * nums[i])
+        min_prod, max_prod = temp_min, temp_max
+        result = max(result, max_prod)
+    return result
+
+
 if __name__ == '__main__':
-    print(max_product([2, 3, -2, 4]))
-    print(max_product([-2, 0, -1, ]))
-    print(max_product([-2, 0, -1, -5]))
+    print(max_product_correct([2, 3, -2, 4]))
+    print(max_product_correct([-2, 0, -1, ]))
+    print(max_product_correct([-2, 0, -1, -5]))
