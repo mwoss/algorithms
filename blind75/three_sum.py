218134679b67ab8cabc77f48c4c6d2b190a6c9eb
mwoss <wos.mateusz16@gmail.com>
2022-06-27 23:41:15 +0200

    Add turbo ineffcient solution for 3sum

---
+++ blind75/three_sum.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def three_sum(nums: List[int]) -> List[List[int]]:
+    if len(nums) <= 2:
+        return []
+
+    result = set()
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    result.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+    return [list(e) for e in result]
+
+
+if __name__ == '__main__':
+    print(three_sum([]))
+    print(three_sum([0]))
+    print(three_sum([-1, 0, 1, 2, -1, 4]))
