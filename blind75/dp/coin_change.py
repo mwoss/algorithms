33ea2e7a37a24528598b068865fe53de5a2895ba
mwoss <wos.mateusz16@gmail.com>
2022-07-12 23:54:43 +0200

    Add solution for coins change

---
+++ blind75/dp/coin_change.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def coin_change(coins: List[int], amount: int) -> int:
+    # the smallest coin is always 1 and there is only single combination that fulfill this condition
+    possibilities = [amount + 1] * (amount + 1)
+    possibilities[0] = 0
+
+    for coin in coins:
+        for curr_amount in range(amount + 1):
+            possibilities[curr_amount] = min(possibilities[curr_amount], possibilities[curr_amount - coin] + 1)
+
+    return possibilities[amount]
+
+
+
+if __name__ == '__main__':
+    print(coin_change([1, 2, 5], 11))
