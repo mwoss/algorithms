c08ccbdefdbf4518336cea01d4f9f01025ded11a
mwoss <wos.mateusz16@gmail.com>
2022-08-15 00:38:00 +0200

    Add iterative solution to invert bin tree task

--- blind75/tree/invert_bin_tree.py
+++ blind75/tree/invert_bin_tree.py
@@ -16,6 +16,26 @@ def invert_tree(root: Optional[TreeNode]) -> Optional[TreeNode]:
     return root
 
 
+def invert_tree_iter(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if root is None:
+        return None
+
+    stack = [root]
+    while stack:
+        node = stack.pop()
+
+        if node.left is not None:
+            stack.append(node.left)
+        if node.right is not None:
+            stack.append(node.right)
+
+        tmp = node.left
+        node.left = node.right
+        node.right = tmp
+
+    return root
+
+
 def print_tree(root: Optional[TreeNode]):
     if root is None:
         return
@@ -26,6 +46,12 @@ def print_tree(root: Optional[TreeNode]):
 
 
 if __name__ == '__main__':
-    tree = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7, TreeNode(6), TreeNode(9)))
-    inv_tree = invert_tree(tree)
-    print_tree(inv_tree)
+    tree1 = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7, TreeNode(6), TreeNode(9)))
+    inv_tree1 = invert_tree(tree1)
+    print_tree(inv_tree1)
+
+    print("--------------------")
+
+    tree2 = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7, TreeNode(6), TreeNode(9)))
+    inv_tree2 = invert_tree_iter(tree2)
+    print_tree(inv_tree2)
