aed50c2f08beec5d08f635766e1e922b8462cf40
mwoss <wos.mateusz16@gmail.com>
2022-09-16 01:33:58 +0200

    Add dp solution for palindromic substring question

--- blind75/string/palindromic_substrings.py
+++ blind75/string/palindromic_substrings.py
@@ -1,16 +1,35 @@
 def count_substrings(s: str) -> int:
-    palindrom_count, str_len = 0, len(s)
+    palindrome_count, str_len = 0, len(s)
     for i in range(str_len):
         for j in range(i + 1, str_len + 1):
             if _is_palindrome(s[i:j]):
-                palindrom_count += 1
-    return palindrom_count
+                palindrome_count += 1
+    return palindrome_count
+
+
+def count_substrings_2(s: str) -> int:
+    palindrome_count, str_len = 0, len(s)
+    palindromes_matrix = [[False] * str_len for _ in range(str_len)]
+
+    for i in range(str_len - 1, -1, -1):
+        for j in range(i, str_len):
+            if s[i] == s[j] and (i - j < 2 or palindromes_matrix[i + 1][j - 1]):
+                palindromes_matrix[i][j] = True
+                palindrome_count += 1
+
+    return palindrome_count
 
 
 def _is_palindrome(s: str) -> bool:
+    # can be optimized by iterating from start and end until meeting in the middle of string
     return s == s[::-1]
 
 
 if __name__ == '__main__':
     print(count_substrings("abc"))
     print(count_substrings("aaa"))
+    print(count_substrings("aaaaaa"))
+
+    print(count_substrings_2("abc"))
+    print(count_substrings_2("aaa"))
+    print(count_substrings_2("aaaaaa"))
