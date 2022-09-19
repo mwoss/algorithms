2ead957a02c3b337678f59c6fbcda5b719ceaea0
mwoss <wos.mateusz16@gmail.com>
2022-09-19 23:10:44 +0200

    Add another solution for palindromic substrings question

--- blind75/string/palindromic_substrings.py
+++ blind75/string/palindromic_substrings.py
@@ -7,6 +7,11 @@ def count_substrings(s: str) -> int:
     return palindrome_count
 
 
+def _is_palindrome(s: str) -> bool:
+    # can be optimized by iterating from start and end until meeting in the middle of string
+    return s == s[::-1]
+
+
 def count_substrings_2(s: str) -> int:
     palindrome_count, str_len = 0, len(s)
     palindromes_matrix = [[False] * str_len for _ in range(str_len)]
@@ -20,16 +25,35 @@ def count_substrings_2(s: str) -> int:
     return palindrome_count
 
 
-def _is_palindrome(s: str) -> bool:
-    # can be optimized by iterating from start and end until meeting in the middle of string
-    return s == s[::-1]
+def count_substrings_3(s: str) -> int:
+    count = 0
+    for i in range(len(s)):
+        count += check_palindrome(s, i, i)
+        count += check_palindrome(s, i, i + 1)
+    return count
+
+
+def check_palindrome(s: str, i: int, j: int) -> int:
+    count = 0
+    while i >= 0 and j < len(s) and s[i] == s[j]:
+        count += 1
+        i -= 1
+        j += 1
+    return count
 
 
 if __name__ == '__main__':
     print(count_substrings("abc"))
     print(count_substrings("aaa"))
     print(count_substrings("aaaaaa"))
+    print("-------------------------")
 
     print(count_substrings_2("abc"))
     print(count_substrings_2("aaa"))
     print(count_substrings_2("aaaaaa"))
+    print("-------------------------")
+
+    print(count_substrings_3("abc"))
+    print(count_substrings_3("aaa"))
+    print(count_substrings_3("aaaaaa"))
+    print("-------------------------")
