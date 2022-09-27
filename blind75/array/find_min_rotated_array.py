b0591c78c665008457765aa9c5a3bcddfb71408c
mwoss <wos.mateusz16@gmail.com>
2022-09-27 23:34:19 +0200

    Add optimal solution for find min in rotated array question

--- blind75/array/find_min_rotated_array.py
+++ blind75/array/find_min_rotated_array.py
@@ -8,11 +8,25 @@ def find_min(nums: List[int]) -> int:
 
 def find_min_optimized(nums: List[int]) -> int:
     # time complexity: O(lgn)
-    # TODO: implement modified bin search
-    pass
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[right] < nums[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return nums[left]
 
 
 if __name__ == '__main__':
     print(find_min([3, 4, 5, 1, 2]))
     print(find_min([4, 5, 6, 7, 0, 1, 2]))
     print(find_min([11, 13, 15, 17]))
+
+    print("------------------")
+
+    print(find_min_optimized([3, 4, 5, 1, 2]))
+    print(find_min_optimized([4, 5, 6, 7, 0, 1, 2]))
+    print(find_min_optimized([11, 13, 15, 17]))
