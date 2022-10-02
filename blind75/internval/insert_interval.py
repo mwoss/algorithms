267d9760bf271b2431dbca954bbe60e94b90859b
mwoss <wos.mateusz16@gmail.com>
2022-10-03 01:24:11 +0200

    Add potentialy optimal solution for insert interval question

--- blind75/internval/insert_interval.py
+++ blind75/internval/insert_interval.py
@@ -2,8 +2,22 @@ from typing import List
 
 
 def insert(intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
-    # skip list sounds like a good fit here
-    pass
+    # solution with returning new list
+    updated_intervals = []
+
+    for start, end in intervals:
+        if end < new_interval[0]:
+            updated_intervals.append([start, end])
+        elif start > new_interval[1]:
+            updated_intervals.append(new_interval)
+            new_interval = [start, end]
+        else:
+            new_interval[0] = min(new_interval[0], start)
+            new_interval[1] = max(new_interval[1], end)
+
+    updated_intervals.append(new_interval)
+
+    return updated_intervals
 
 
 if __name__ == '__main__':
