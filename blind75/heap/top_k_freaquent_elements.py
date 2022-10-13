20ba7d4449398ea4228804414833789c815c76f5
mwoss <wos.mateusz16@gmail.com>
2022-10-13 23:22:44 +0200

    Add heap based solution for top k frequent elements

---
+++ blind75/heap/top_k_freaquent_elements.py
@@ -0,0 +1,22 @@
+import heapq
+from collections import Counter
+from typing import List
+
+
+def top_k_frequent(nums: List[int], k: int) -> List[int]:
+    most_frequent, nums_count = [], Counter(nums)
+
+    for num, count in nums_count.items():
+        heapq.heappush(most_frequent, (count, num))
+
+        if most_frequent[0][1] == num and len(most_frequent) > k:
+            # we can do it, because it is guaranteed that the answer is unique, so if the "smallest" element in heap
+            # is equal to current element it means it's for sure that element, and we can safely remove it
+            heapq.heappop(most_frequent)
+
+    return [num for _, num in most_frequent]
+
+
+if __name__ == '__main__':
+    print(top_k_frequent([1, 1, 1, 2, 2, 3], 2))
+    print(top_k_frequent([1], 1))
