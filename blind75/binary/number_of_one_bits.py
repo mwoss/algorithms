5a384dbec1c76862e6af848aed388f5b09edf249
mwoss <wos.mateusz16@gmail.com>
2022-11-14 00:35:37 +0100

    Add even more optimized solution for hamming weight task

--- blind75/binary/number_of_one_bits.py
+++ blind75/binary/number_of_one_bits.py
@@ -3,19 +3,36 @@ def hamming_weight(n: int) -> int:
 
 
 def hamming_weight_optimized(n: int) -> int:
-    result = 0
+    ones = 0
     while n > 0:
-        if n & 1:
-            result += 1
+        ones = ones + (n & 1)
         n = n >> 1
-    return result
+    return ones
+
+
+def hamming_weight_more_optimized(n: int) -> int:
+    ones = 0
+    while n > 0:
+        n = n & (n - 1)
+        ones += 1
+    return ones
 
 
 if __name__ == '__main__':
+    print("-- hamming_weight --")
     print(hamming_weight(3))
     print(hamming_weight(256))
     print(hamming_weight(356))
+    print(hamming_weight(1253267))
 
+    print("-- hamming_weight_optimized --")
     print(hamming_weight_optimized(3))
     print(hamming_weight_optimized(256))
     print(hamming_weight_optimized(356))
+    print(hamming_weight_optimized(1253262))
+
+    print("-- hamming_weight_more_optimized --")
+    print(hamming_weight_more_optimized(3))
+    print(hamming_weight_more_optimized(256))
+    print(hamming_weight_more_optimized(356))
+    print(hamming_weight_more_optimized(1253262))
