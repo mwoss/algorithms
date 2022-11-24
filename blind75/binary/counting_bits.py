1e542d2b63d2616d4f1d4b54029c8f392dc37ef7
mwoss <wos.mateusz16@gmail.com>
2022-11-24 23:01:01 +0100

    Add O(n) solution for counting bits task

--- blind75/binary/counting_bits.py
+++ blind75/binary/counting_bits.py
@@ -8,6 +8,18 @@ def count_bits(n: int) -> List[int]:
     return results
 
 
+def count_bits_optimized(n: int) -> List[int]:
+    results = [0] * (n + 1)  # we skip 0 as we already know it has zero "1s"
+    for i in range(1, n + 1):
+        results[i] = results[i // 2] + i % 2
+    return results
+
+
 if __name__ == '__main__':
     print(count_bits(2))
     print(count_bits(5))
+
+    print("================")
+
+    print(count_bits_optimized(2))
+    print(count_bits_optimized(5))
