b0100da7f2c082668936e72f165ea796fc3559e7
mwoss <wos.mateusz16@gmail.com>
2022-12-04 23:25:45 +0100

    Add another implementation of missing number problem

--- blind75/binary/missing_number.py
+++ blind75/binary/missing_number.py
@@ -9,7 +9,19 @@ def missing_number(nums: List[int]) -> int:
     return missing
 
 
+def missing_number_2(nums: List[int]) -> int:
+    n = len(nums)  # 0 is not counted
+    current_sum = sum(nums)
+    full_sum = n * (n + 1) // 2
+
+    return full_sum - current_sum
+
+
 if __name__ == '__main__':
     print(missing_number([3, 0, 1]))
     print(missing_number([0, 1]))
     print(missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+
+    print(missing_number_2([3, 0, 1]))
+    print(missing_number_2([0, 1]))
+    print(missing_number_2([9, 6, 4, 2, 3, 5, 7, 0, 1]))
