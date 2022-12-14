a57486cb8d65587a23fd00315888b19223e59bd1
mwoss <wos.mateusz16@gmail.com>
2022-12-14 23:53:50 +0100

    Add optimized solution for length of longest inc subsequence

--- blind75/dp/longest_increasing_subsequence.py
+++ blind75/dp/longest_increasing_subsequence.py
@@ -1,7 +1,8 @@
+from bisect import bisect_left
 from typing import List
 
 
-def length_of_list(nums: List[int]) -> int:
+def length_of_lis(nums: List[int]) -> int:
     n = len(nums)
     dp = [1] * n
     for i in range(n):
@@ -11,7 +12,23 @@ def length_of_list(nums: List[int]) -> int:
     return max(dp)
 
 
+def length_of_lis_optimized(nums: List[int]) -> int:
+    subsequence = []
+    for num in nums:
+        if len(subsequence) == 0 or subsequence[-1] < num:
+            subsequence.append(num)
+        else:
+            idx = bisect_left(subsequence, num)
+            subsequence[idx] = num
+
+    return len(subsequence)
+
+
 if __name__ == '__main__':
-    print(length_of_list([10, 9, 2, 5, 3, 7, 101, 18]))
-    print(length_of_list([0, 1, 0, 3, 2, 3]))
-    print(length_of_list([7, 7, 7, 7, 7, 7, 7]))
+    print(length_of_lis([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(length_of_lis([0, 1, 0, 3, 2, 3]))
+    print(length_of_lis([7, 7, 7, 7, 7, 7, 7]))
+
+    print(length_of_lis_optimized([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(length_of_lis_optimized([0, 1, 0, 3, 2, 3]))
+    print(length_of_lis_optimized([7, 7, 7, 7, 7, 7, 7]))
