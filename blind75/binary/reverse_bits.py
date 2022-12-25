2dab68e2f4bd6ff850916f94834f0c7d268bb9ff
mwoss <wos.mateusz16@gmail.com>
2022-12-25 23:47:59 +0100

    Add probably optimal but still hacky  solution to reverse bits question

--- blind75/binary/reverse_bits.py
+++ blind75/binary/reverse_bits.py
@@ -5,6 +5,21 @@ def reverse_bits(n: int) -> int:
     return int(reversed_bits, 2)
 
 
+def reverse_bits_optimized(n: int) -> int:
+    if n == 0:
+        return 0
+
+    result = 0
+    for i in range(32):
+        result = result << 1
+        result += n & 1
+        n = n >> 1
+    return result
+
+
 if __name__ == '__main__':
     print(reverse_bits(43261596))
     print(reverse_bits(4294967293))
+
+    print(reverse_bits_optimized(43261596))
+    print(reverse_bits_optimized(4294967293))
