0518945402442df00183774405440f6320803fb1
mwoss <wos.mateusz16@gmail.com>
2023-02-24 00:19:17 +0100

    Add dp solution for longest palindromic substring

--- blind75/string/longest_palindromic_substring.py
+++ blind75/string/longest_palindromic_substring.py
@@ -1,4 +1,7 @@
 def longest_palindrome_brute(s: str) -> str:
+    if len(s) < 2:
+        return s
+
     longest = ''
     for i in range(0, len(s)):
         for j in range(i, len(s)):
@@ -11,6 +14,30 @@ def _is_palindrome(s: str):
     return s == s[::-1]
 
 
+def longest_palindrome(s: str) -> str:
+    if len(s) < 2:
+        return s
+
+    str_len, longest = len(s), s[0]
+
+    dp = [[False for _ in range(str_len)] for _ in range(str_len)]
+
+    for i in range(str_len):
+        dp[i][i] = True
+
+    for i in range(0, str_len - 1):
+        for j in range(i + 1, str_len):
+            if s[i] == s[j] and (dp[i + 1][j - 1] or j - i == 1):
+                dp[i][j] = True
+                if len(longest) < j - i + 1:
+                    longest = s[i:j + 1]
+
+    return longest
+
+
 if __name__ == '__main__':
     print(longest_palindrome_brute('babad'))
     print(longest_palindrome_brute('cbbd'))
+
+    print(longest_palindrome('babad'))
+    print(longest_palindrome('cbbd'))
