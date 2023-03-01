3bbf8bad13fd569082bb6d5bba1faf220dbc5ccd
mwoss <wos.mateusz16@gmail.com>
2023-03-01 23:07:31 +0100

    Thinking about solution for longest repeating char replacement

---
+++ blind75/string/longest_repeating_char_replecement.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def character_replacement(s: str, k: int) -> int:
+    c = Counter(s)
+
+    pass
+
+def _find_longest(s: str):
+    pass
+
+if __name__ == '__main__':
+    print(character_replacement("ABAB", 2))
+    print(character_replacement("AABABBA", 1))
