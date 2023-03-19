65fe20b1b63d741dd16d9abd706ed08bd0eef9de
mwoss <wos.mateusz16@gmail.com>
2023-03-19 23:54:55 +0100

    Add memory optimized solution for jump game problem

--- blind75/dp/jump_game.py
+++ blind75/dp/jump_game.py
@@ -13,6 +13,20 @@ def can_jump(nums: List[int]) -> bool:
     return reachable[-1]
 
 
+def can_jump_optimized(nums: List[int]) -> bool:
+    # memory optimzied solution
+    n = len(nums) - 1
+
+    furthest_reachable = 0
+    for idx in range(0, n - 1):
+        furthest_reachable = max(furthest_reachable, idx + nums[idx])
+
+    return furthest_reachable == n
+
+
 if __name__ == '__main__':
     print(can_jump([2, 3, 1, 1, 4]))
     print(can_jump([3, 2, 1, 0, 4]))
+
+    print(can_jump_optimized([2, 3, 1, 1, 4]))
+    print(can_jump_optimized([3, 2, 1, 0, 4]))
