872b325f909738deeb04e30fd90bb986dc9f77bf
mwoss <wos.mateusz16@gmail.com>
2024-03-10 00:38:51 +0100

    Add solution to interview question - target sum

---
+++ interviews/c/target_sum.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from typing import List, Tuple
+
+
+def target_sum(nums: List[int], target: int) -> Tuple[int, int]:
+    number_to_index = {}
+    for idx, num in enumerate(nums):
+        to_find = target - num
+        if to_find in number_to_index:
+            return number_to_index[to_find], idx
+        number_to_index[num] = idx
+
+    raise Exception(f"There's no number pair that sums up to {target}")
+
+
+def target_sum_non_repeats(nums: List[int], target: int) -> Tuple[int, int]:
+    number_to_index = defaultdict(set)
+    for curr_idx, num in enumerate(nums):
+        to_find = target - num
+
+        if to_find in number_to_index:
+            first_sum_part_idx = number_to_index[to_find].pop()
+            yield first_sum_part_idx, curr_idx
+
+        number_to_index[num].add(curr_idx)
+
+
+if __name__ == '__main__':
+    print(target_sum([1, 2, 6, 4], 5))
+    print(target_sum([1, 2, 6, 4, 4], 5))
+
+    print(list(target_sum_non_repeats([1, 1, 2, 6, 4, 4], 5)))
