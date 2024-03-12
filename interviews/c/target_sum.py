57295128c9f3179b549c1064ae722710310ad32f
mwoss <wos.mateusz16@gmail.com>
2024-03-12 13:50:35 +0100

    Adjust target sum solution

--- interviews/c/target_sum.py
+++ interviews/c/target_sum.py
@@ -18,11 +18,15 @@ def target_sum_non_repeats(nums: List[int], target: int) -> Tuple[int, int]:
     for curr_idx, num in enumerate(nums):
         to_find = target - num
 
-        if to_find in number_to_index:
+        is_target_found = False
+
+        if to_find in number_to_index and number_to_index[to_find]:
             first_sum_part_idx = number_to_index[to_find].pop()
+            is_target_found = True
             yield first_sum_part_idx, curr_idx
 
-        number_to_index[num].add(curr_idx)
+        if is_target_found is False:
+            number_to_index[num].add(curr_idx)
 
 
 if __name__ == '__main__':
@@ -30,3 +34,4 @@ if __name__ == '__main__':
     print(target_sum([1, 2, 6, 4, 4], 5))
 
     print(list(target_sum_non_repeats([1, 1, 2, 6, 4, 4], 5)))
+    print(list(target_sum_non_repeats([5, 5, 5, 5, 5], 10)))
