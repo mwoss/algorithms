12602c4ee6599770009186788105077cd5957332
mwoss <wos.mateusz16@gmail.com>
2024-03-12 14:08:34 +0100

    Add optimized solution to top k frequent elements

--- blind75/heap/top_k_freaquent_elements.py
+++ blind75/heap/top_k_freaquent_elements.py
@@ -9,12 +9,10 @@ def top_k_frequent(nums: List[int], k: int) -> List[int]:
     for num, count in nums_count.items():
         heapq.heappush(most_frequent, (count, num))
 
-        if most_frequent[0][1] == num and len(most_frequent) > k:
-            # we can do it, because it is guaranteed that the answer is unique, so if the "smallest" element in heap
-            # is equal to current element it means it's for sure that element, and we can safely remove it
+        if len(most_frequent) > k:
             heapq.heappop(most_frequent)
 
-    return [num for _, num in most_frequent]
+    return [heapq.heappop(most_frequent)[1] for _ in range(k)]
 
 
 def top_k_frequent_2(nums: List[int], k: int) -> List[int]:
@@ -24,9 +22,33 @@ def top_k_frequent_2(nums: List[int], k: int) -> List[int]:
     return [num for _, num in most_frequent]
 
 
+def top_k_frequent_optimized(nums: List[int], k: int) -> List[int]:
+    # bucket sort approach
+    nums_count = Counter(nums)
+    buckets = [[] for _ in range(len(nums) + 1)]
+
+    for num, count in nums_count.items():
+        buckets[count].append(num)
+
+    result = []
+    for i in range(len(nums), -1, -1):
+        bucket = buckets[i]
+        for freq_num in bucket:
+            if len(result) == k:
+                return result
+            result.append(freq_num)
+
+    return result
+
+
 if __name__ == '__main__':
     print(top_k_frequent([1, 1, 1, 2, 2, 3], 2))
+    print(top_k_frequent([1, 1, 1, 2, 2, 3, 4, 4, 4], 2))
     print(top_k_frequent([1], 1))
 
     print(top_k_frequent_2([1, 1, 1, 2, 2, 3], 2))
     print(top_k_frequent_2([1], 1))
+
+    print(top_k_frequent_optimized([1, 1, 1, 2, 2, 3], 2))
+    print(top_k_frequent_optimized([1, 1, 1, 2, 2, 3, 4, 4, 4], 2))
+    print(top_k_frequent_optimized([1], 1))
